69140a14dd2e5ac62935fcf66dc756f7afa93d65
ChoChiMoo <eddyojh@gmail.com>
2023-10-12 05:56:45 +0000

    chap4 prob1 finish

---
+++ chap4/prob1/cat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*print txt files on stdout*/
+
+int main(int argc, char *argv[]){
+	FILE *fp;
+	int c;
+	if (argc < 2)
+		fp = stdin;
+	else fp = fopen(argv[1],"r");
+
+	c = getc(fp);
+	while (c != EOF){
+		putc(c, stdout);
+		c = getc(fp);
+	}
+	fclose(fp);
+	return 0;
+}
