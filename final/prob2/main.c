80b3dca310f6cfedb1dc15d65e2ba08d13efd8e7
ChoChiMoo <eddyojh@gmail.com>
2023-12-14 14:55:18 +0900

    final

---
+++ final/prob2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+
+//every process pirnt "Hello" once
+
+int main(void){//p0
+	int pid1
+	pid1 = fork();
+	
+
+
+
+
+
+	return 0;
+}
